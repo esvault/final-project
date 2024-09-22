5bca099690f6da069a5e36f911532dcdae725550
OlegKechin <oleg_kecin@mail.ru>
2024-09-23 02:47:00 +0300

    amendment BuildAnimal

--- src/main/java/org/example/buildEntity/BuildAnimal.java
+++ src/main/java/org/example/buildEntity/BuildAnimal.java
@@ -2,23 +2,23 @@ package org.example.buildEntity;
 
 import org.example.entity.Animal;
 
-public class BuildAnimal implements Builder {
+public class BuildAnimal  {
     private String species;
     private String eyeColor;
     private String wool;
 
-    @Override
-    public void setFirstParam(String species) {
+
+    public void setSpecies(String species) {
         this.species = species;
     }
 
-    @Override
-    public void setSecondParam(String eyeColor) {
+
+    public void setEyeColor(String eyeColor) {
         this.eyeColor = eyeColor;
     }
 
-    @Override
-    public void setLastParam(String wool) {
+
+    public void setWool(String wool) {
         this.wool = wool;
     }
 
--- src/main/java/org/example/director/DirectorRandom.java
+++ src/main/java/org/example/director/DirectorRandom.java
@@ -1,16 +1,22 @@
 package org.example.director;
 
+import org.example.buildEntity.BuildAnimal;
 import org.example.buildEntity.Builder;
 
 import java.util.Random;
 import java.util.Scanner;
-
+// Избавиться от интерфейса в реализации билдера (ничего не понятно в директоре)
+//создать массивы с вариантами значений стринговых полей ()
 public class DirectorRandom {
+    String[] animalSpecies = {"kat","dog","cow","snake"};
+    String[] animalEyeColor = {"blue","black","gray","green"};
+    String[] animalWool = {"long","short","not","feather"};
     Random rand = new Random();
-    public void createRandomAnimal(Builder builder){
-            builder.setFirstParam(rand.nextInt(10));
-            builder.setSecondParam(scanner.nextLine());
-            builder.setLastParam(scanner.nextLine());
+    public void createRandomAnimal(BuildAnimal builder){
+            builder.setSpecies(animalSpecies[rand.nextInt(animalSpecies.length)]);
+            builder.setEyeColor(animalEyeColor[rand.nextInt(animalEyeColor.length)]);
+            builder.setWool(animalWool[rand.nextInt(animalWool.length)]);
+            builder.createAnimal();
     }
     public void createRandomBarrel(Builder builder) {
             builder.setFirstParam(scanner.nextLine());
