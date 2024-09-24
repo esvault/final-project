a9132e34e3e322b781bebae409e661718daa389a
OlegKechin <oleg_kecin@mail.ru>
2024-09-24 13:58:33 +0300

    Исправил директор на бул

--- src/main/java/org/example/director/Director.java
+++ src/main/java/org/example/director/Director.java
@@ -12,7 +12,7 @@ public class Director {
         try (Scanner scanner = new Scanner(System.in)) {
             builder.setSpecies(scanner.nextLine());
             builder.setEyeColor(scanner.nextLine());
-            builder.setWool(scanner.nextLine());
+            builder.setWool(scanner.nextBoolean());
         }
     }
     public void createBarrel(BuildBarrel builder) {
