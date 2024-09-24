8fb09bf8f8840e979cdfa9c5682691d62f2b52df
OlegKechin <oleg_kecin@mail.ru>
2024-09-24 13:05:53 +0300

    add RandomFillStrategy full

--- src/main/java/org/example/model/RandomFillStrategy.java
+++ src/main/java/org/example/model/RandomFillStrategy.java
@@ -2,6 +2,7 @@ package org.example.model;
 
 import org.example.buildEntity.BuildAnimal;
 import org.example.buildEntity.BuildBarrel;
+import org.example.buildEntity.BuildHuman;
 import org.example.director.DirectorRandom;
 import org.example.entity.Animal;
 import org.example.entity.Barrel;
@@ -12,13 +13,13 @@ import java.util.Scanner;
 
 //TODO Implement class
 public class RandomFillStrategy implements FillStrategy {
-
+    DirectorRandom directorRandom = new DirectorRandom();
     @Override
     public Animal[] fillArrayByAnimals() {
         BuildAnimal buildAnimal = new BuildAnimal();
-        DirectorRandom directorRandom = new DirectorRandom();
         Animal[] animals;
         try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите количество создоваемых элементов");
             animals = new Animal[scanner.nextInt()];
             for (int i = 0; i < animals.length; i++) {
                 directorRandom.createRandomAnimal(buildAnimal);
@@ -31,11 +32,31 @@ public class RandomFillStrategy implements FillStrategy {
 
     @Override
     public Barrel[] fillArrayByBarrels() {
-        return null;
+        BuildBarrel buildBarrel = new BuildBarrel();
+        Barrel[] barrels;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите количество создоваемых элементов");
+            barrels = new Barrel[scanner.nextInt()];
+            for (int i = 0; i < barrels.length; i++) {
+                directorRandom.createRandomBarrel(buildBarrel);
+                barrels[i] = buildBarrel.createBarrel();
+            }
+        }
+        return barrels;
     }
 
     @Override
     public Human[] fillArrayByHumans() {
-        return null;
+        BuildHuman buildHuman = new BuildHuman();
+        Human[] humans;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите количество создоваемых элементов");
+            humans = new Human[scanner.nextInt()];
+            for (int i = 0; i < humans.length; i++) {
+                directorRandom.createRandomHuman(buildHuman);
+                humans[i] = buildHuman.createHuman();
+            }
+        }
+        return humans;
     }
 }
