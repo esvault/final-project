ea3aca09f019c2bcdbd7dc4e1962f6d385e7f6aa
Ivan Azarnikov <54893829+esvault@users.noreply.github.com>
2024-09-25 23:56:48 +0300

    Added ability to sort array and find element, language of output changed

--- src/main/java/org/example/ui/Console.java
+++ src/main/java/org/example/ui/Console.java
@@ -1,7 +1,11 @@
 package org.example.ui;
 
+import org.example.algorithms.BinarySearch;
 import org.example.algorithms.InsertionSort;
 import org.example.algorithms.SortingStrategy;
+import org.example.buildEntity.BuildAnimal;
+import org.example.buildEntity.BuildBarrel;
+import org.example.buildEntity.BuildHuman;
 import org.example.entity.Animal;
 import org.example.entity.Barrel;
 import org.example.entity.Human;
@@ -13,6 +17,8 @@ import org.example.model.*;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static org.example.model.UserFillStrategy.*;
 
 public class Console {
     private ConsoleState state = ConsoleState.NO_ARRAY;
@@ -37,7 +43,7 @@ public class Console {
 
     private ReturnCode fillArray() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter array filling way (1 - random, 2 - from file, 3 - by user)");
+        System.out.println("Выберите способ заполнения массива (1 - случайно, 2 - из файла, 3 - пользовательский)");
 
         Integer userResponse = sc.nextInt();
         validateFillArrayInput(userResponse);
@@ -49,7 +55,7 @@ public class Console {
                 String filePath = null;
                 File file = null;
                 while (isNotFound){
-                    System.out.println("Enter file path");
+                    System.out.println("Введите путь к файлу");
                     filePath = sc.next();
                     file = new File(filePath);
                     if(!file.exists()){
@@ -65,7 +71,7 @@ public class Console {
             default: throw new IllegalArgumentException("Несуществующая команда");
         }
 
-        System.out.println("Type of elements (1 - Animal, 2 - Barrel, 3 - Human)");
+        System.out.println("Введите тип элементов (1 - Animal, 2 - Barrel, 3 - Human)");
         int elementTypeCode = sc.nextInt();
 
         switch (elementTypeCode) {
@@ -76,13 +82,14 @@ public class Console {
             default: throw new IllegalArgumentException("Несуществующий вариант ответа");
         }
 
-        System.out.println("Your array: " + Arrays.toString(currentArray));
+        System.out.println("Ваш массив: " + Arrays.toString(currentArray));
         state = ConsoleState.ARRAY_INITIALIZED;
         return ReturnCode.OK;
     }
 
     private ReturnCode askAction() {
-        System.out.println("Choose action to be executed (1 - sort, 2 - binary search, 3 - new array, 4 - exit)");
+        System.out.println("Выберите действие, которое хотите исполнить (1 - отсортировать, 2 - выполнить бинарный поиск, 3 - создать новый массив, 4 - выйти)");
+//        System.out.println("Choose action to be executed (1 - sort, 2 - binary search, 3 - new array, 4 - exit)");
         Scanner sc = new Scanner(System.in);
         int askResponse = sc.nextInt();
 
@@ -93,31 +100,23 @@ public class Console {
                     return ReturnCode.ERROR;
                 }
 
-                if (currentArray[0] instanceof Animal) {
-                    SortingStrategy<Animal> ss = new InsertionSort<>();
-                    ss.sort((Animal[]) currentArray);
-                } else if (currentArray[0] instanceof Barrel) {
-                    SortingStrategy<Barrel> ss = new InsertionSort<>();
-                    ss.sort((Barrel[]) currentArray);
-                } else if (currentArray[0] instanceof Human) {
-                    SortingStrategy<Human> ss = new InsertionSort<>();
-                    ss.sort((Human[]) currentArray);
-                } else {
-                    System.out.println("Unsupported type");
-                    return ReturnCode.ERROR;
-                }
+                SortingStrategy<SupportedTypes> ss = new InsertionSort<>();
+                ss.sort(currentArray);
 
                 state = ConsoleState.ARRAY_SORTED;
+                System.out.println("Your sorted array: " + Arrays.toString(currentArray));
                 break;
             }
             case 2: {
                 if (state != ConsoleState.ARRAY_SORTED) {
-                    System.out.println("Sort array before applying binary search");
+                    System.out.println("Прежде чем выполнить бинарный поиск, отсортируйте массив");
                     return ReturnCode.ERROR;
                 }
                 // выполнить бинарный поиск
-                System.out.println("Enter element to be search");
-//                BinarySearch.search(currentArray, )
+                System.out.println("Введите данные для поиска");
+                SupportedTypes elem = askDataForBinSearch();
+                int binSearchResult = BinarySearch.search(currentArray, elem);
+                System.out.println("Индекс искомого элемента: " + binSearchResult);
                 break;
             }
             case 3: {
@@ -133,6 +132,38 @@ public class Console {
         }
 
         return ReturnCode.OK;
+    }
+
+    private SupportedTypes askDataForBinSearch() {
+        SupportedTypes searchedElement;
+        System.out.print("Введите данные по объекту, который хотите найти");
+        if (currentArray[0] instanceof Animal) {
+            BuildAnimal builder = new BuildAnimal();
+            builder.setSpecies(validateStringInput("Введите вид животного: "));
+            builder.setEyeColor(validateStringInput("Введите цвет глаз: "));
+            builder.setWool(validateBooleanInput("Есть ли шерсть? (да/нет): "));
+
+            searchedElement = builder.createAnimal();
+        } else if (currentArray[0] instanceof Barrel) {
+            BuildBarrel builder = new BuildBarrel();
+            builder.setVolume(validateIntInput("Введите объем бочки (в литрах): "));
+            builder.setContent(validateStringInput("Введите хранимый материал: "));
+            builder.setMaterial(validateStringInput("Введите материал, из которого изготовлена бочка: "));
+
+            searchedElement = builder.createBarrel();
+        } else if (currentArray[0] instanceof Human) {
+            BuildHuman builder = new BuildHuman();
+            builder.setGender(validateStringInput("Введите пол: "));
+            builder.setAge(validateIntInput("Введите возраст: "));
+            builder.setSurname(validateStringInput("Введите фамилию: "));
+
+            searchedElement = builder.createHuman();
+        } else {
+            searchedElement = null;
+        }
+
+
+        return searchedElement;
     }
 
     private void validateFillArrayInput(Integer response) {
