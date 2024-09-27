4cc9e1cfe31e04e7643b4ca86cda8b51a895ec36
Ivan Azarnikov <54893829+esvault@users.noreply.github.com>
2024-09-27 18:26:54 +0300

    updated README.md

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,10 @@
 package org.example;
 
-import org.example.model.RandomFillStrategy;
-import org.example.model.UserFillStrategy;
 import org.example.ui.Console;
 
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-       // (new Console()).runMainLoop();
-UserFillStrategy FillStrategy = new UserFillStrategy();
-        System.out.println(Arrays.toString(
-        FillStrategy.fillArrayByAnimals()));
-
+        (new Console()).runMainLoop();
     }
 }
